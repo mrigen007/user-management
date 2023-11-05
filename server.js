8cbe5b19c95244aef3b10a3d341e6fdd141d80d5
mrigendra <mrigen2022@gmail.com>
2023-11-05 19:41:14 +0530

    removed comments and app file

--- server.js
+++ server.js
@@ -22,29 +22,4 @@ const startDB = async () => {
     }
 };
 
-
-// const app = require('./app');
-// const http = require('http')
-// const port = process.env.PORT || 3000;
-// const connectDB = require("./database/connect");
-// const server = http.createServer(app);
-// const color = require('colorette');
-
-// // 👇️ handle uncaught exceptions
-// process.on('uncaughtException', function (err) {
-//     console.log(color.red(`Uncaught Exception Error Occurred ${err}`));
-// });
-
-// const startDB = async () => {
-//     try {
-//         await connectDB(process.env.MONGO_URL);
-//         console.log(color.blue(`DB server is connected`));
-//         server.listen(() => {
-//             console.log(color.blue(`Node server is running on port ${port}...`));
-//         })
-//     } catch (error) {
-//         console.log(color.red(`ERROR is ${error}`));
-//     }
-// };
-
 startDB();
